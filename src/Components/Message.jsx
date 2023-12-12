cb1727003958d8f5fe95bebaaa42f249f9832196
Jeevan Jyoti Choudhury <jeevanjyoti549@gmail.com>
2023-12-12 19:31:55 +0530

    message section added

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,10 +1,27 @@
-import { Box } from '@chakra-ui/react'
+import { Text, Flex, Heading, Button } from '@chakra-ui/react'
 import React from 'react'
+import { BsChatRightTextFill } from "react-icons/bs";
+import { PiPaperPlaneRightFill } from "react-icons/pi";
+
 
 const Message = () => {
   return (
-    <Box h="80vh">Message</Box>
-  )
+    <Flex h="80vh" justify="center" align="center" flexDirection={"column"}>
+      <BsChatRightTextFill size="40px" />
+      <Heading size="lg" my={5}>
+        No messages
+      </Heading>
+      <Text>Messages from the team will be shown here</Text>
+      <Button
+        rightIcon={<PiPaperPlaneRightFill color="white" size={20} />}
+        colorScheme="blue"
+        variant="solid"
+        mt={10}
+      >
+        Send us a message
+      </Button>
+    </Flex>
+  );
 }
 
 export default Message
